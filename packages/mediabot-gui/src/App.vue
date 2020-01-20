26ecd35db99cee71897d5051b70da37b0dce6088
Artem Aginskiy <aaginskiy@gmail.com>
2020-01-19 21:58:00 -0600

    build(gui): Set up skeleton Vue gui project

---
+++ packages/mediabot-gui/src/App.vue
@@ -0,0 +1,84 @@
+<template>
+  <v-app>
+    <v-navigation-drawer
+      app
+      dark
+      mini-variant
+      mini-variant-width="56"
+      permanent
+    >
+      <v-list
+          dense
+          nav
+          class="py-0"
+        >
+          <v-list-item two-line>
+            <v-list-item-avatar>
+              <img src="https://randomuser.me/api/portraits/men/81.jpg">
+            </v-list-item-avatar>
+
+            <v-list-item-content>
+              <v-list-item-title>Application</v-list-item-title>
+              <v-list-item-subtitle>Subtext</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+
+          <v-divider></v-divider>
+
+          <v-list-item
+            link
+            to="/movies"
+          >
+            <v-list-item-icon>
+              <v-icon>mdi-movie</v-icon>
+            </v-list-item-icon>
+
+            <v-list-item-content>
+              <v-list-item-title>Movies</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+
+          <v-list-item
+            link
+            to="/jobs"
+          >
+            <v-list-item-icon>
+              <v-icon>mdi-checkbox-multiple-marked-circle-outline</v-icon>
+            </v-list-item-icon>
+
+            <v-list-item-content>
+              <v-list-item-title>Jobs</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+
+          <v-list-item
+            link
+          >
+            <v-list-item-icon>
+              <v-icon>mdi-settings</v-icon>
+            </v-list-item-icon>
+
+            <v-list-item-content>
+              <v-list-item-title>Settings</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+    </v-navigation-drawer>
+    <v-content>
+      <router-view />
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
---
+++ packages/mediabot-gui/src/views/Movies.vue
@@ -0,0 +1,172 @@
+<template>
+  <v-container fluid pa-0>
+    <v-app-bar dark>
+      <v-toolbar-title>Movies</v-toolbar-title>
+    </v-app-bar>
+    <v-row
+      class="pa-0"
+      style="height: calc(100vh - 64px); max-height: calc(100% - 64px);"
+    >
+      <v-col cols="3" class="pa-0 overflow-y-auto" style="height: 100%">
+        <v-navigation-drawer>
+          <v-list class="pa-0">
+            <v-list-item
+              v-for="movie in movies"
+              :key="movie._id"
+              link
+              :to="{ name: 'movies', params: { id: movie._id } }"
+            >
+              <v-list-item-title v-text="movie.title"></v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-navigation-drawer>
+      </v-col>
+       <v-col cols="9">
+        <v-card
+          v-for="movie in movies"
+          :key="movie._id"
+          link
+          :to="{ name: 'movies', params: { id: movie._id } }"
+        >
+          <v-img
+            class="white--text align-end"
+            height="200px"
+            src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+          >
+            <v-card-title>Top 10 Australian beaches</v-card-title>
+          </v-img>
+
+          <v-card-subtitle class="pb-0">Number 10</v-card-subtitle>
+
+          <v-card-text class="text--primary">
+            <div>Whitehaven Beach</div>
+
+            <div>Whitsunday Island, Whitsunday Islands</div>
+          </v-card-text>
+
+          <v-card-actions>
+            <v-btn color="orange" text>
+              Share
+            </v-btn>
+
+            <v-btn color="orange" text>
+              Explore
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+      <v-col cols="9">
+        <v-card
+          v-for="movie in movies"
+          :key="movie._id"
+          link
+          :to="{ name: 'movies', params: { id: movie._id } }"
+        >
+          <v-img
+            class="white--text align-end"
+            height="200px"
+            src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+          >
+            <v-card-title>Top 10 Australian beaches</v-card-title>
+          </v-img>
+
+          <v-card-subtitle class="pb-0">Number 10</v-card-subtitle>
+
+          <v-card-text class="text--primary">
+            <div>Whitehaven Beach</div>
+
+            <div>Whitsunday Island, Whitsunday Islands</div>
+          </v-card-text>
+
+          <v-card-actions>
+            <v-btn color="orange" text>
+              Share
+            </v-btn>
+
+            <v-btn color="orange" text>
+              Explore
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import { useFind } from 'feathers-vuex'
+import { computed } from '@vue/composition-api'
+
+export default {
+  name: 'Movies',
+  components: {
+    // MessageList
+  },
+  props: ['id'],
+  setup(props, context) {
+    const { Movie } = context.root.$FeathersVuex.api
+    // const { $store } = context.root
+
+    // Users
+    // const user = computed(() => $store.state.auth.user)
+    // const usersParams = computed(() => {
+    //   return {
+    //     query: {
+    //       $sort: { email: 1 },
+    //       $limit: 25
+    //     }
+    //   }
+    // })
+    // const { items: users } = useFind({ model: User, params: usersParams })
+
+    // Messages
+    const moviesParams = computed(() => {
+      return {
+        query: {
+          $sort: { createdAt: 1 },
+          $limit: 25
+        }
+      }
+    })
+    const { items: movies } = useFind({
+      model: Movie,
+      params: moviesParams
+    })
+
+    // // Logout
+    // function logout() {
+    //   return $store.dispatch('auth/logout')
+    // }
+
+    return {
+      // user,
+      movies
+    }
+  }
+}
+</script>
+
+<style scoped>
+main#chat {
+  height: 100%;
+}
+
+/* Header */
+header.title-bar {
+  padding: 10px 0;
+  border-bottom: 1px solid #f1f1f1;
+}
+
+header.title-bar img.logo {
+  width: 100%;
+  max-width: 140px;
+}
+
+header.title-bar span.title {
+  color: #969696;
+  font-weight: 100;
+  text-transform: uppercase;
+  font-size: 1.2em;
+  margin-left: 7px;
+}
+</style>
